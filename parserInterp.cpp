ad9f69fa3c2996df5547831307f838d1de8ec6e4
JEER11 <127362314+JEER11@users.noreply.github.com>
2025-04-26 16:45:53 -0400

    Upload SADAL interpreter project files

---
+++ parserInterp.cpp
@@ -0,0 +1,53 @@
+/* Implementation of Interpreter
+	for the Simple Ada-Like (SADAL) Language
+ * parser.cpp
+ * Programming Assignment 3
+ * Spring 2025
+*/
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include "parserInterp.h"
+
+
+map<string, bool> defVar;
+map<string, Token> SymTable;
+map<string, Value> TempsResults; //Container of temporary locations of Value objects for results of expressions, variables values and constants 
+
+vector<string> * Ids_List;
+
+namespace Parser {
+	bool pushed_back = false;
+	LexItem	pushed_token;
+
+	static LexItem GetNextToken(istream& in, int& line) {
+		if( pushed_back ) {
+			pushed_back = false;
+			return pushed_token;
+		}
+		return getNextToken(in, line);
+	}
+
+	static void PushBackToken(LexItem & t) {
+		if( pushed_back ) {
+			abort();
+		}
+		pushed_back = true;
+		pushed_token = t;	
+	}
+
+}
+
+static int error_count = 0;
+
+int ErrCount()
+{
+    return error_count;
+}
+
+void ParseError(int line, string msg)
+{
+	++error_count;
+	cout << line << ": " << msg << endl;
+}
+
